885b4335718903dc35b727f646406bb1ceb2ada4
单守臣 <shansc@mshutech.com>
2023-05-11 14:44:36 +0800

    修改部分文案

--- services/errorService.ts
+++ services/errorService.ts
@@ -17,14 +17,7 @@ const useErrorService = () => {
               code: error.status || 'unknown',
               messageLines: error.statusText
                 ? [error.statusText]
-                : [
-                    t(
-                      'Make sure your OpenAI API key is set in the bottom left of the sidebar.',
-                    ),
-                    t(
-                      'If you completed this step, OpenAI may be experiencing issues.',
-                    ),
-                  ],
+                : ['请联系管理员(shanshouchen@gmail.com)'],
             } as ErrorMessage);
       },
       [t],
